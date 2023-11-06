8eb8f4b22710c2025f366d37126f5a7058c20b26
Dominik Żydek <dominik.zydek@edu.uekat.pl>
2023-11-06 15:47:15 +0100

    added layout and main, todo: handling clicks

--- src/CalculatorGUI.java
+++ src/CalculatorGUI.java
@@ -1,8 +1,9 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class CalculatorGUI extends JFrame {
+public class CalculatorGUI extends JFrame implements ActionListener{
     private JTextField textField;
     private JButton[] numberBtns;
     private JButton addBtn, subBtn, mulBtn, divBtn, eqBtn, clrBtn;
@@ -19,5 +20,64 @@ public class CalculatorGUI extends JFrame {
         textField.setFont(new Font("Arial", Font.PLAIN, 24));
         textField.setHorizontalAlignment(JTextField.RIGHT);
         add(textField, BorderLayout.NORTH);
+
+        numberBtns = new JButton[10];
+        for (int i = 0; i < 10; i++) {
+            numberBtns[i] = new JButton(String.valueOf(i));
+            numberBtns[i].setFont(new Font("Arial", Font.PLAIN, 24));
+            numberBtns[i].addActionListener(this);
+        }
+
+        addBtn = new JButton("+");
+        addBtn.setFont(new Font("Arial", Font.PLAIN, 24));
+        addBtn.addActionListener(this);
+        subBtn = new JButton("-");
+        subBtn.setFont(new Font("Arial", Font.PLAIN, 24));
+        subBtn.addActionListener(this);
+        mulBtn = new JButton("*");
+        mulBtn.setFont(new Font("Arial", Font.PLAIN, 24));
+        mulBtn.addActionListener(this);
+        divBtn = new JButton("/");
+        divBtn.setFont(new Font("Arial", Font.PLAIN, 24));
+        divBtn.addActionListener(this);
+        eqBtn = new JButton("=");
+        eqBtn.setFont(new Font("Arial", Font.PLAIN, 24));
+        eqBtn.addActionListener(this);
+        clrBtn = new JButton("C");
+        clrBtn.setFont(new Font("Arial", Font.PLAIN, 24));
+        clrBtn.addActionListener(this);
+
+        JPanel buttonPanel = new JPanel();
+        buttonPanel.setLayout(new GridLayout(4, 4));
+
+        buttonPanel.add(numberBtns[1]);
+        buttonPanel.add(numberBtns[2]);
+        buttonPanel.add(numberBtns[3]);
+        buttonPanel.add(addBtn);
+        buttonPanel.add(numberBtns[4]);
+        buttonPanel.add(numberBtns[5]);
+        buttonPanel.add(numberBtns[6]);
+        buttonPanel.add(subBtn);
+        buttonPanel.add(numberBtns[7]);
+        buttonPanel.add(numberBtns[8]);
+        buttonPanel.add(numberBtns[9]);
+        buttonPanel.add(mulBtn);
+        buttonPanel.add(clrBtn);
+        buttonPanel.add(numberBtns[0]);
+        buttonPanel.add(eqBtn);
+        buttonPanel.add(divBtn);
+
+        add(buttonPanel, BorderLayout.CENTER);
+
+        setVisible(true);
     }
+
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> new CalculatorGUI());
+    }
+
 }
